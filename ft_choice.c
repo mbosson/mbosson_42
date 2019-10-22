333e11267c5f5ece5fc47a740345218c54b02992
Matheo Bosson <mbosson@z3r9p2.le-101.fr>
2019-10-22 16:32:41 +0200

    Choice sans bug

--- ft_choice.c
+++ ft_choice.c
@@ -6,7 +6,7 @@
 /*   By: mbosson <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/18 13:52:12 by mbosson      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/21 18:49:33 by mbosson     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/22 16:31:34 by mbosson     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -18,7 +18,7 @@ int		empty_buffer(void)
 	int c;
 
 	c = 0;
-	while (c != '\n' && c != EOF)
+	while (c != '\n' && c != EOF && c != '\r')
 		c = getchar();
 	return (0);
 }
@@ -40,17 +40,11 @@ int		ft_strchr(char *str, char c)
 int		test_stdin(char *str, int choice_max)
 {
 	if (str[1] != 0)
-	{
-		empty_buffer();
 		return (0);
-	}
 	while ((str[0] - 48) != choice_max)
 	{
 		if (choice_max == 0)
-		{
-			empty_buffer();
 			return (0);
-		}
 		choice_max--;
 	}
 	return (1);
@@ -67,10 +61,11 @@ int		choice(char *str, int size, int choice_max, char *print)
 			printf("%s : ", print);
 		if (fgets(str, size, stdin) != NULL)
 		{
+			
 			ft_strchr(str, '\n');
-			if (str[0] != 0 && str[0] - 48 <= choice_max && str[1] == 0)
+			if (str[0] != 0 && (str[0] - 48) <= choice_max && str[0] >= '1' && str[1] == 0)
 				test_bug = 1;
-			else if (str[0] != 0)
+			else if (str[0] != 0 && str[1] != 0)
 				empty_buffer();
 		}
 	}
