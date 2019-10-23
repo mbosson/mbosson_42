e098476b53379fea5232434d7d8a63695bb948fd
Matheo Bosson <mbosson@z1r2p2.le-101.fr>
2019-10-23 17:03:21 +0200

    Voila c'est beau

---
+++ ft_choice.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_choice.c                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: mbosson <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/10/18 13:52:12 by mbosson      #+#   ##    ##    #+#       */
+/*   Updated: 2019/10/22 17:40:29 by mbosson     ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "header.h"
+
+int		empty_buffer(void)
+{
+	int c;
+
+	c = 0;
+	while (c != '\n' && c != EOF && c != '\r')
+		c = getchar();
+	return (0);
+}
+
+int		ft_strchr(char *str, char c)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != 0)
+	{
+		if (str[i] == c)
+			str[i] = 0;
+		i++;
+	}
+	return (1);
+}
+
+int		choice(int size, int choice_max, char *print)
+{
+	int test_bug;
+
+	test_bug = 0;
+	while (test_bug != 1)
+	{
+		if (print != 0)
+			printf("%s : ", print);
+		if (fgets(chemin, size, stdin) != NULL)
+		{
+			
+			ft_strchr(chemin, '\n');
+			if (chemin[0] != 0 && (chemin[0] - 48) <= choice_max && chemin[0] >= '1' && chemin[1] == 0)
+				test_bug = 1;
+			else if (chemin[0] != 0 && chemin[1] != 0)
+				empty_buffer();
+		}
+	}
+	return (1);
+}
+
+void	print(char *str)
+{
+	char enter;
+
+	enter = 0;
+	printf("%s", str); 
+	while (enter != '\n' && enter != '\r')
+		enter = getchar();
+}
---
+++ main.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   main.c                                           .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: mbosson <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/10/18 13:10:50 by mbosson      #+#   ##    ##    #+#       */
+/*   Updated: 2019/10/22 17:43:07 by mbosson     ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "header.h"
+
+int main(void)
+{
+	print("Ce jeux est un jeux à choix multiples. Vos choix et vos actes auront des consequences, et vous en etes le seul responsable. ");
+	print("Lorsque qu'un choix se presente a vous, rentrez le chiffre correspondant et appuyez sur Entree");
+	choice(3, 2, "Avez vous bien compris ? Oui (1) / Non (2)");
+	if (chemin[0] == '2')
+	{
+		print("Tant pis");
+		return (0);
+	}
+	print("Tres bien, nous allons pouvoir commencer");
+	reveil();
+	return (0);
+}
