ab6b4ab3859542cbb9763ecd03b183183bb318a8
Cecile Bosson <cecilebosson@MacBook-Air-de-Cecile.local>
2019-10-27 23:45:49 +0100

    get next line

---
+++ my_get_next_line/ft_get_next_line.c
@@ -0,0 +1,13 @@
+int get_next_line(int fd, char **line)
+{
+	int	i;
+	int 	malloc_value;
+	char	*result;
+
+	i = 0;
+	while (line[i][0])
+		i++;
+	while(i)
+
+		result = malloc(sizeof(char) * malloc_value
+}
